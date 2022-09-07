fd7b5130b3c80bc6a3e6e93e2ae9d1b8ec74244f
mohammad_lbf <labbaf2016@gmail.com>
2022-09-07 16:51:56 +0430

    about us feature developed

--- src/components/main pages/AboutDeveloper.js
+++ src/components/main pages/AboutDeveloper.js
@@ -9,9 +9,46 @@ const AboutDeveloper = () => {
         })
     } , [])
     return (
-        <div style={{minHeight:"80vh"}}>
-
+        <div style={{marginTop:"85px"}} className="pt-3 pt-lg-5">
+        <div className="container rounded">
+        <div>
+            <p className="lh-lg text-end text-end fs-13">
+            : مشخصات طراح و توسعه دهنده پروژه
+            <br />
+            <br />
+            نام و نام خانوادگی : محمد لبافی
+            <br />
+            <br />
+            : مهارت ها
+            <br />
+            </p>
+            <p className="lh-lg mt-2 text-start fs-13 fs-sm-15 ">
+            HTML & CSS
+                <br/>
+                Java Script (ES 6)
+                <br />
+                bootstrap 5
+                <br />
+                SASS
+                <br />
+                Git & Git hub
+                <br />
+                React js (React hooks & functional)
+                <br/>
+                react router dom (SPA site)
+                <br />
+                Redux
+                <br />
+                GraphQL
+                <br />
+                responsive design
+                <br />
+                material UI
+                <br />
+            </p>
         </div>
+        </div>
+    </div>
     );
 };
 
